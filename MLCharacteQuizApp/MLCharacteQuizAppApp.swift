52b376ec646a21ee93e1a625846c7d7181764e93
David-ShinDJ <dnrur124@gmail.com>
2022-08-27 21:29:34 +0900

    프로젝트시작

---
+++ MLCharacteQuizApp/MLCharacteQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLCharacteQuizAppApp.swift
+//  MLCharacteQuizApp
+//
+//  Created by David_ADA on 2022/08/27.
+//
+
+import SwiftUI
+
+@main
+struct MLCharacteQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MLCharacteQuizApp/ModelView/ContentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentViewModel.swift
+//  MLCharacteQuizApp
+//
+//  Created by David_ADA on 2022/08/27.
+//
+
+import Foundation
